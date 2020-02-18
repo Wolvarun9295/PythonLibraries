3892a5e3fd8aa6793591e3b6088b93a20a2aaf31
Varun Nagrare <varun.nagrare@gmail.com>
2020-02-18 10:06:56 +0530

    REFACTORED: Listing all files in a Directory by taking input from User

--- Basic Python/ListAllFilesInDir.py
+++ Basic Python/ListAllFilesInDir.py
@@ -1,9 +1,12 @@
 # Imported OS module.
 import os
 
-# Taking user input.
-listAll = input('Enter the absolute path of directory to list all files: ')
-# Listing all the files in directory.
-printList = os.listdir(listAll)
-for i in printList:
-    print(i)
+try:
+    # Taking user input.
+    listAll = input('Enter the absolute path of directory to list all files: ')
+    # Listing all the files in directory.
+    printList = os.listdir(listAll)
+    for i in printList:
+        print(i)
+except:
+    print('Invalid Input! Try Again!')
