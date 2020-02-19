0093ebeb0901ced4807064589b8c6523ef0a6966
Varun Nagrare <varun.nagrare@gmail.com>
2020-02-19 11:03:47 +0530

    REFACTORED: Program to add leading 0's to a string

--- Basic Python/AddLeadingZeros.py
+++ Basic Python/AddLeadingZeros.py
@@ -2,7 +2,7 @@
 string = input('Enter a number: ')
 # Taking the count from user.
 countOfZeros = int(input('Enter number of zeros to be added after it: '))
-# Using ljust to append the 0's to the string.
+# Using zfill to append the 0's to the string.
 # NOTE: len(string) is added to countOfZeros to avoid the error of skipping positions when adding 0 after string.
 string = string.zfill(countOfZeros + len(string))
 print(string)
